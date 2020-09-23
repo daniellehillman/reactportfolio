5a53e327524e4033c9f674457ffc1c81ba694d06
Danielle Hillman <hillm108@mail.chapman.edu>
2020-09-22 22:09:50 -0700

    test

---
+++ reactportfolio/src/pages/Contact/Contact.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Contact = () => {
+  return ( 
+      <Router>
+         <section class="jumbotron">
+    <h1>Contact Page</h1>      
+    <p>Add me on my social media or leave an email below! </p>
+   
+            <section id="contactsTop">
+              <ul class="social-network social-circle">
+                <li><a href="mailto:hillm108@mail.chapman.edu" target="_blank" class="icoRss" title="Rss"><i class="fa fa-envelope"></i></a></li>
+                <li><a href="https://www.facebook.com/danielle.hillman.92" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://twitter.com/danihillman5" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.instagram.com/dani_hillman/" target="_blank" class="icoGoogle" title="Google +"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/daniellehillman5/" target="_blank" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/daniellehillman" target="_blank" class="icoGithub" title="Github"><i class="fa fa-github"></i></a></li>
+              <li><a href="./Resume-Danielle HillmanBootcamp.pdf" target="_blank" class="icoGithub" title="Github"><i class="fa fa-file"></i></a></li>
+              </ul>
+            </section>
+        </section>
+ 
+
+ {/* <section class="container"/>
+     <hr class="light"/>
+<section class="mb-4"/> 
+
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Leave me a Message</h2>
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact me directly.</p>
+
+    <section class="row"/>
+        <section class="col-md-9 mb-md-0 mb-5"/>
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST"/>
+
+                <section class="row"/>
+                    <div class="col-md-6"/>
+                        <section class="md-form mb-0"/>
+                            <input id="name" name="name" class="form-control" placeholder="Your Name"/>
+                            <label for="name" class=""></label>
+                        </section>
+                    </div>
+    
+                    <div class="col-md-6">
+                        <section class="md-form mb-0">
+                            <input id="email" name="email" class="form-control" placeholder="Your Email">
+                            <label for="name" class=""></label>
+                        </section>
+                    </div>
+                  
+
+                </section>
+            
+                <section class="row">
+                    <div class="col-md-12">
+                        <section class="md-form mb-0">
+                            <input id="subject" name="subject" class="form-control" placeholder="Subject">
+                            <label for="name" class=""></label>
+                        </section>
+                    </div>
+                </section>
+               <section class="row">
+                    <div class="col-md-12">
+
+                        <section class="md-form">
+                            <textarea  id="message" name="message" rows="2" class="form-control md-textarea" placeholder="Your Message"></textarea>
+                        </section>
+
+                    </div>
+                </section>
+            </form>
+        <br> */}
+      </Router>
+
+  
+  )
+}
+
+export default Contact
